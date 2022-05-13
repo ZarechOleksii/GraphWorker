c1b342f16de96200ef529271eb85570af8ef6644
Oleksii Zarechanskyi <misterz06022002@gmail.com>
2022-05-13 21:14:37 +0300

    finalized dijkstra method and added status messages

--- src/WebApp/wwwroot/js/methods.js
+++ src/WebApp/wwwroot/js/methods.js
@@ -60,13 +60,17 @@ function add_vertice() {
 
     simulation.nodes(data.nodes)
     simulation.alpha(0.1).restart()
+
+    document.getElementById("status").innerHTML = `<b><p>Added vertex ${id}.</b></p>`;
 }
 
 async function add_connection(this_button) {
     this_button.style.backgroundColor = "green";
     clear_selected();
+    clear_result();
     document.getElementById("cancel_button").disabled = false;
     enable_selection();
+    document.getElementById("status").innerHTML = `<b><p>Select two points (connecting from first to second).</b></p>`;
 
     const someTimeoutAction = () => {
         return new Promise((resolve) => {
@@ -91,8 +95,10 @@ async function add_connection(this_button) {
 async function dijkstra(this_button) {
     this_button.style.backgroundColor = "green";
     clear_selected();
+    clear_result();
     document.getElementById("cancel_button").disabled = false;
     enable_selection();
+    document.getElementById("status").innerHTML = `<b><p>Select two points (shortest path from first to second).</b></p>`;
 
     const someTimeoutAction = () => {
         return new Promise((resolve) => {
@@ -134,8 +140,6 @@ function dijkstra_logic() {
     let first_id = selected[0];
     let second_id = selected[1];
 
-    let text = "";
-
     let dataAlgo = data.nodes.map(node => ({
         id: node.id,
         passed: false,
@@ -149,25 +153,34 @@ function dijkstra_logic() {
     let current = first;
     current.weight = 0;
 
+    let text = `<b>Algorithm steps:</b><p>Set weight of all points to infinity, starting from point ${current.id}, weight 0.</p><ol>`;
+
     while (current != null) {
+        text += `<li>Setting new weights from point ${current.id}.<ol>`;
+
         current.connections.forEach(function (con) {
             let secondPoint = dataAlgo.find(node => node.id == con.target.id);
 
             if (!secondPoint.passed) {
                 if (secondPoint.weight > current.weight + con.weight) {
+                    text += `<li>Setting new weight for ${secondPoint.id}: from ${secondPoint.weight} to ${current.weight} + ${con.weight} = ${current.weight + con.weight}</li>`
                     secondPoint.weight = current.weight + con.weight;
                     secondPoint.pathing = Array.from(current.pathing);
                     secondPoint.pathing.push(current.id);
                 }
             }
         });
+
         current.passed = true;
         let filtered = dataAlgo
-            .filter(q => !q.passed);
+            .filter(q => !q.passed && q.weight != Infinity);
+
         if (filtered.length > 0) {
             current = filtered.reduce((first, second) => first.weight > second.weight ? second : first);
+            text += `</ol><p>New minimal point is ${current.id}, weight ${current.weight}.</p></li>`;
         }
         else {
+            text += "</ol></li></ol><p>All available points are passed</p>";
             current = null;
         }
     }
@@ -175,20 +188,39 @@ function dijkstra_logic() {
     let toWhere = dataAlgo.find(node => node.id == second_id);
     toWhere.pathing.push(toWhere.id);
 
-    let currWeight = 0;
-
-    text += "<ol>";
-    for (let i = 0; i < toWhere.pathing.length - 1; i++) {
-        let conn = data.links.find(link => link.source.id == toWhere.pathing[i] && link.target.id == toWhere.pathing[i + 1]);
-        text += `<li>From ${toWhere.pathing[i]} to ${toWhere.pathing[i + 1]}: ${currWeight} + ${conn.weight} = ${currWeight += conn.weight}</li>`;
-    }
-    text += "</ol>";
-
     if (toWhere.weight != Infinity) {
-        set_status(text + `<p>Shortest path from vertex ${first.id} to vertex ${toWhere.id} is ${toWhere.weight} units.</p>`);
+        let currWeight = 0;
+
+        text += "<p>Entire path summary:</p><ol>";
+        for (let i = 0; i < toWhere.pathing.length - 1; i++) {
+            let conn = data.links.find(link => link.source.id == toWhere.pathing[i] && link.target.id == toWhere.pathing[i + 1]);
+            text += `<li>From ${toWhere.pathing[i]} to ${toWhere.pathing[i + 1]}: ${currWeight} + ${conn.weight} = ${currWeight += conn.weight}</li>`;
+        }
+        text += "</ol>";
+        text = `<p><b>Shortest path from vertex ${first.id} to vertex ${toWhere.id} is ${toWhere.weight} units.</b></p>` + text;
+        set_status(text);
+
+        nodes
+            .filter(function (d) {
+                return toWhere.pathing.includes(d.id);
+            })
+            .select("circle")
+            .classed("result", true);
+
+        lines
+            .filter(function (d) {
+                for (let i = 0; i < toWhere.pathing.length - 1; i++) {
+                    if (d.source.id == toWhere.pathing[i] && d.target.id == toWhere.pathing[i + 1]) {
+                        return true;
+                    }
+                }
+                return false;
+            })
+            .style("stroke", "red");
     }
     else {
-        set_status(`There is no way to get from vertex ${first.id} to vertex ${toWhere.id}.`);
+        text = `<p><b>There is no way to get from vertex ${first.id} to vertex ${toWhere.id}.</b></p>` + text;
+        set_status(text);
     }
 }
 
@@ -198,6 +230,7 @@ function add_connection_logic() {
     let weight = 30;
 
     if (data.links.find(q => q.source.id == first_id && q.target.id == second_id) != null) {
+        document.getElementById("status").innerHTML = `<b><p>Connection from ${first_id} to ${second_id} already exists.</b ></p>`;
         return;
     }
 
@@ -283,10 +316,12 @@ function add_connection_logic() {
         .merge(labels);
     simulation.nodes(data.nodes)
     simulation.alpha(0.1).restart()
+    document.getElementById("status").innerHTML = `<b><p>Successfully connected ${first_id} to ${second_id}.</b ></p>`;
 }
 
 function cancel_button() {
     cancel = true;
+    document.getElementById("status").innerHTML = "<b><p>Canceled operation.</b></p>";
 }
 
 function clear_selected() {
@@ -300,6 +335,14 @@ function clear_selected() {
         .on("click", click);
 }
 
+function clear_result() {
+    circles
+        .classed("result", false);
+
+    lines
+        .style("stroke", "#aaa");
+}
+
 function enable_selection() {
     d3
         .selectAll(".node")
@@ -307,17 +350,19 @@ function enable_selection() {
 }
 
 function select_this(event, d) {
-    d3
-        .select(this)
-        .select(".circle")
-        .classed("selected", true);
+    if (!selected.includes(d.id)) {
+        d3
+            .select(this)
+            .select(".circle")
+            .classed("selected", true);
 
-    d.fx = null;
-    d.fy = null;
-    d3.select(this).select("circle").classed("fixed", false);
-    simulation.alpha(1).restart();
+        d.fx = null;
+        d.fy = null;
+        d3.select(this).select("circle").classed("fixed", false);
+        simulation.alpha(1).restart();
 
-    selected.push(d.id);
+        selected.push(d.id);
+    }
 }
 
 function set_status(text) {
